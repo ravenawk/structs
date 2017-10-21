51a51a5f42095e1d634d22ae63349f756b29da41
Pat Martin <23747724+ravenawk@users.noreply.github.com>
2017-10-21 13:10:26 -0700

    Added recievers for structs

--- main.go
+++ main.go
@@ -2,15 +2,33 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Jameson",
+		contactInfo: contactInfo{
+			email:   "jim.j@jim.net",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
